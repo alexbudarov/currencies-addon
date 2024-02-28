df977b2a044015bfea4933656518e66d339fda63
budarov <budarov@haulmont.com>
2024-02-28 19:19:00 +0400

    backend API for currency conversion

---
+++ currencies/src/main/java/com/sample/currencies/conversion/ConversionService.java
@@ -0,0 +1,31 @@
+package com.sample.currencies.conversion;
+
+import com.sample.currencies.app.Currency;
+import com.sample.currencies.app.CurrencyRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+@Service
+public class ConversionService {
+
+    private final CurrencyRepository currencyRepository;
+
+    public ConversionService(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+    /**
+     * Convert given amount to a specified currency
+     * @param amount money amount
+     * @param currencyId currency ID
+     * @return converted amount
+     */
+    public BigDecimal convertAmount(BigDecimal amount, Long currencyId) {
+        Currency currency = currencyRepository.findById(currencyId).orElseThrow();
+        var mc = new MathContext(2, RoundingMode.HALF_EVEN);
+        return amount.divide(currency.getConversionRate(), mc);
+    }
+}
